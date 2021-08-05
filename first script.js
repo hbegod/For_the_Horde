d7464c124a851957321dd35c499b551c76884a5a
Eprst <easter_p@mail.ru>
2021-08-05 11:48:27 +0300

    лоигны через вопросик

--- first script.js
+++ first script.js
@@ -1,6 +1,7 @@
-let a = Number(prompt("Задайте a",""));
-let b = Number(prompt("Задайте b",""));
-let result = (a+b < 4) ? 'Мало' : 'Много';
-alert(result);
+let login = prompt("Введите логин", "");
 
+let message = (login == 'Сотрудник') ? 'Привет' :
+              (login == 'Директор') ? 'Здравствуйте' :
+              (login == '') ? 'Нет логина': '';
 
+alert(message);
